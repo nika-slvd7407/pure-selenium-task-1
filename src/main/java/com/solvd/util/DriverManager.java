b5ad97d9719e3551a2664f947f3571fe3463ad10
nika7407 <ntsalkalamanidze@solvd.com>
2026-02-16 20:04:56 +0400

    task#2 pr1 fixes added Driver manager and ensured local thread driver version
    Signed-off-by: nika7407 <ntsalkalamanidze@solvd.com>

---
+++ src/main/java/com/solvd/util/DriverManager.java
@@ -0,0 +1,20 @@
+package com.solvd.util;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverManager {
+
+    private static final ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
+
+    public static void setDriver(WebDriver driver) {
+        driverThreadLocal.set(driver);
+    }
+
+    public static WebDriver getDriver() {
+        return driverThreadLocal.get();
+    }
+
+    public static void removeDriver() {
+        driverThreadLocal.remove();
+    }
+}
