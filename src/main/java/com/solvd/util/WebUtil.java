9726574daafcfe3aa5addae56bc0684bac644b9d
nika7407 <ntsalkalamanidze@solvd.com>
2026-02-18 15:54:42 +0400

    task#1 pr3 fixes
    Signed-off-by: nika7407 <ntsalkalamanidze@solvd.com>

---
+++ src/main/java/com/solvd/util/WebUtil.java
@@ -0,0 +1,17 @@
+package com.solvd.util;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static com.solvd.util.DriverManager.getDriver;
+
+public class WebUtil {
+
+    public static void switchFrame(WebDriver driver, String id, WebDriverWait wait){
+        WebElement iframe = wait.until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
+        driver.switchTo().frame(iframe);
+    }
+}
