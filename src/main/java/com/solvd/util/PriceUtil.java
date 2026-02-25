88009ec11992620a2248fd9e3f8f53f86b3dc43d
nika7407 <ntsalkalamanidze@solvd.com>
2026-02-25 16:40:31 +0400

    #task-5 pr fixes #2 carried
    Signed-off-by: nika7407 <ntsalkalamanidze@solvd.com>

---
+++ src/main/java/com/solvd/util/PriceUtil.java
@@ -0,0 +1,14 @@
+package com.solvd.util;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class PriceUtil {
+
+    public static Double addTaxAndRound(double price){
+        double priceWithTax = price * 1.20;
+        BigDecimal rounded = BigDecimal.valueOf(priceWithTax).setScale(2, RoundingMode.HALF_UP);
+        return rounded.doubleValue();
+    }
+
+}
