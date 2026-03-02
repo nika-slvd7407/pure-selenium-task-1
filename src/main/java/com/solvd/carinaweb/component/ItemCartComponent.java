c00857595b8b91e5b4e6297225f6e38a67a0b205
nika7407 <ntsalkalamanidze@solvd.com>
2026-03-02 11:46:52 +0400

    #task-5 pr fixes #4
    Signed-off-by: nika7407 <ntsalkalamanidze@solvd.com>

--- src/main/java/com/solvd/carinaweb/component/ItemCartComponent.java
+++ src/main/java/com/solvd/carinaweb/component/ItemCartComponent.java
@@ -3,6 +3,7 @@ package com.solvd.carinaweb.component;
 import com.solvd.carinaweb.page.CheckoutPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ItemCartComponent extends BaseComponent {
@@ -15,14 +16,14 @@ public class ItemCartComponent extends BaseComponent {
     public CheckoutPage clickProceedToCheckout() {
 
         By checkoutLocator = By.cssSelector(".modal-body .cart-content-btn .btn-primary");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(checkoutLocator));
-        getDriver().findElement(checkoutLocator).click();
+        WebElement checkoutBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(checkoutLocator));
+        checkoutBtn.click();
         return new CheckoutPage(getDriver());
     }
 
     public void clickContinueShopping() {
         By continueLocator = By.cssSelector(".modal-body .cart-content-btn .btn-secondary");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(continueLocator));
-        getDriver().findElement(continueLocator).click();
+        WebElement continiueBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(continueLocator));
+        continiueBtn.click();
     }
 }
